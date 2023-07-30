112bf74b712e7bb05518e8a08d14ec050e278110
Marcelo Rezin <marcelo.rezin@gmail.com>
2023-07-30 11:37:46 -0300

    Salva o CSV no diretório versionado

--- adicionar-mesoregiao.py
+++ adicionar-mesoregiao.py
@@ -8,4 +8,4 @@ df2 = df.merge(dfMesoregioes, left_on='DESCRICAO_MUNICIPIO', right_on='NOME_SEF'
 
 df2 = df2.drop(columns=['INDEX','COD','COD_MUNICIPIO','DESCRICAO_MUNICIPIO','COD_SEF', 'NOME_SEF'])
 
-df2.to_csv('arquivos-brutos/estabelecimentos-sc-municipios-mesoregiao.csv', index=False)
+df2.to_csv('arquivos/estabelecimentos-sc-municipios-mesoregiao.csv', index=False)
